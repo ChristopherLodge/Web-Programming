c82670a020be53692685b96cdc59012b91b1d7b7
Christopher Lodge <chris.lodge.102@gmail.com>
2014-11-09 14:22:57 +0000

    DB Connection File
    This file will be used to form a connection to the database, and will be
    stored outside of htdocs for security purposes.

---
+++ PHP/DBConnect.php
@@ -0,0 +1,7 @@
+<?php
+//store outside document root 
+	$server = "mysql.ccacolchester.com";
+	$user = "christopherl3022";
+	$pass = "1433022";
+	$db = mysqli_connect($server, $user, $pass, "christopherl3022");		
+?>
