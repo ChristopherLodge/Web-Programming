fe31a716deebe80e88ccd23e3487556c202b1715
Christopher Lodge <chris.lodge.102@gmail.com>
2014-11-10 15:48:52 +0000

    Comment Creation Error Handling
    experimental error handling, to be verified once mySQL server is running
    again.

--- PHP/createcomment.php
+++ PHP/createcomment.php
@@ -14,7 +14,7 @@ else if  ($_POST['recipientid'] == $_POST['senderid'])
 }
 else if ($_SESSION[user_id] != $_POST['senderid'])
 {
-	echo "detected hack attempt";
+	echo "detected hack attempt"; //the comment must be coming from the right person! 
 }
 else
 {
@@ -23,8 +23,18 @@ else
 	$content = htmlentities($_POST['contentid']); //catch content from form and strip html
 
 	$stmt = mysqli_prepare($db, "INSERT INTO Comment (SenderID, RecipientID, Content) VALUES (?, ?,?)"); //prepare statement
-	mysqli_stmt_bind_param($stmt, 'sss', $sender, $recipient, $content); //define parameters
-	mysqli_stmt_execute($stmt); //execute the "safe" statement
+	
+	if (!mysqli_stmt_bind_param($stmt, 'sss', $sender, $recipient, $content)) //define parameters
+	{
+		echo "binding failed"; //investigate if correct/for error command
+		exit();
+	}
+	else if (!mysqli_stmt_execute($stmt)) //execute the "safe" statement, print error if any
+	{
+		echo "failure"; //investigate if correct/ for error command
+		exit();
+	}
+
 }
 mysqli_close ($db);
 ?>
