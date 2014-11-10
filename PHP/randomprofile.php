7f09f317f5a76913c830c7759eb95253b1108297
Christopher Lodge <chris.lodge.102@gmail.com>
2014-11-10 09:53:46 +0000

    Random Profile ID Generator
    Fixed a few typos, and performed some local testing. Ensured that it
    worked.

--- PHP/randomprofile.php
+++ PHP/randomprofile.php
@@ -1,20 +1,22 @@
 <?php
-function randomprofile()
-require_once('DBConnect.php');
+function randomprofile() //selects a random facebook user id for random image on index
 {
-	if (mysqli_connect_errno()) 
+	require_once('DBConnect.php'); //conect to db
+	if (mysqli_connect_errno())  //connection erorr handle 
 				{
 				  echo "Failed to connect to MySQL: " . mysqli_connect_error();
 				  exit();
 				}	
-	$query ="SELECT * FROM `User` WHERE FacebookUserID >= (SELECT FLOOR( MAX(FacebookUserID) * RAND()) FROM `User` ) ORDER BY FacebookUserID LIMIT 1"; //double check
-	if (!mysqli_query($db, $query))
+	$query ="SELECT FacebookUserID FROM `User` WHERE FacebookUserID >= (SELECT FLOOR( MAX(FacebookUserID) * RAND()) FROM `User` ) ORDER BY FacebookUserID LIMIT 1"; //selects a random (and rounded) entry from the total listings in the table. It will only select one. 
+	if (!mysqli_query($db, $query)) //if query fails
 				{
 					printf("Error message: %s\n", mysqli_error($db)); //show error
 					mysqli_close($db); //close link
 					exit(); //discontinue script
 				}
-				$result = mysqli_query($db, $query); 
-				echo $result;
+				$result = mysqli_query($db, $query);  //set result
+				$row = mysqli_fetch_array($result); //convert to array
+				$imageid = $row[0]; //user-friendly naming convention
+				echo $imageid; //prints the id for use in a url
 }
 ?>
