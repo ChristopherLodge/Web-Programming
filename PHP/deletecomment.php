256a8e3a418d6e83011d98039b0b1ab52476402e
Christopher Lodge <chris.lodge.102@gmail.com>
2014-11-14 10:38:27 +0000

    Validation of Comments
    Created some simple validation for use on comment script. (strip html
    tags, compare lengths of content, and ensure ids are numerical)

--- PHP/deletecomment.php
+++ PHP/deletecomment.php
@@ -9,9 +9,9 @@ if (!checkuserstatus ())
 }
 else
 {
-	$commentid = htmlentities($_POST['commentid']); //receive comment ID from selection
-	$stmt = mysqli_prepare($db, "DELETE FROM Comment WHERE RecipientID= ". $_SESSION[user_id] . " AND CommentID= ?"); //prepare statement
-	mysqli_stmt_bind_param($stmt, 'i', $commentid); //define parameters
+	$commentid = strip_tags($_POST['commentid']); //receive comment ID from selection
+	$stmt = mysqli_prepare($db, "DELETE FROM Comment WHERE RecipientID= ? AND CommentID= ?"); //prepare statement
+	mysqli_stmt_bind_param($stmt, 'si',  $_SESSION[user_id], $commentid); //define parameters
 	mysqli_stmt_execute($stmt); //execute the "safe" statement
 }
 mysqli_close ($db);
--- PHP/randomprofile.php
+++ PHP/randomprofile.php
@@ -10,7 +10,7 @@ function randomprofile() //selects a random facebook user id for random image on
 	$query ="SELECT FacebookUserID FROM `User` WHERE FacebookUserID >= (SELECT FLOOR( MAX(FacebookUserID) * RAND()) FROM `User` ) ORDER BY FacebookUserID LIMIT 1"; //selects a random (and rounded) entry from the total listings in the table. It will only select one. 
 	if (!mysqli_query($db, $query)) //if query fails
 				{
-					printf("Error message: %s\n", mysqli_error($db)); //show error
+					printf("Error message: %s", mysqli_error($db)); //show error
 					mysqli_close($db); //close link
 					exit(); //discontinue script
 				}
